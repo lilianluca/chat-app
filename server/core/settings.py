d55c7a211e5e3d51c68684f15120d9a3be12d7ba
Lilian Luca <lilianluca2118@gmail.com>
2026-04-09 21:04:00 +0200

    Remove dbsqlite3

--- server/core/settings.py
+++ server/core/settings.py
@@ -73,12 +73,7 @@ CHANNEL_LAYERS = {
 }
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 AUTH_PASSWORD_VALIDATORS = [
